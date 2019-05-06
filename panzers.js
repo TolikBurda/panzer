31dafee57b8de4e6232f36c576ec58bee05e17f3
TolikBurda <tolik.burda@gmail.com>
2019-05-06 17:02:32 +0300

    Create panzers.js

---
+++ panzers.js
@@ -0,0 +1,126 @@
+class Panzer {
+    constructor(w, h, inputMap){
+        
+        this.panzer = {x: 1, y: 1};
+        this.currentDirection = {x: 0, y: 0};
+        this.fieldWidth = w;
+        this.fieldHeight = h;
+        
+    }
+    createPanzer(){
+
+    }
+
+    movePanzer(){
+        this.panzer.x = this.panzer.x + this.currentDirection.x;
+        this.panzer.y = this.panzer.y + this.currentDirection.y;
+    }
+    
+    changeDirectionOfPanzer(keyCode){
+        var directionMap = {
+            up : { x: 0, y: -1},
+            down: { x: 0, y: 1},
+            right: { x: 1, y: 0},
+            left: { x: -1, y: 0}
+        }   
+
+    }
+    shooting(){
+
+    }
+    
+}
+const panzer = new Panzer()
+// class Shell {
+//     constructor(){
+
+//     }
+// }
+class Field {
+    constructor(){
+        this.arrOfBlocks = [];
+    }
+}
+class Game {
+    constructor(){
+        this.arrOfPanzers = [];
+        
+
+        this.superInputMap = [
+            {
+                38 : 'up',
+                40 : 'down',
+                39 : 'right',
+                37 : 'left'
+            },
+            {
+                87 : 'up',
+                83 : 'down',
+                68 : 'right',
+                65 : 'left'
+            }
+        ]
+    }
+
+    panzerControl(){
+        document.addEventListener('keydown', (e)=> {
+            for(let i = 0; i < this.arrOfPanzers.length; i++){
+                this.arrOfPanzers[i].changeDirectionOfPanzer(e.keyCode)
+            }
+        })
+    }
+
+    mainLoop(){
+
+    }
+}
+
+
+class Drawing {
+    constructor(){
+        this.canvas = null;
+        this.createHtmlElements();
+        this.ctx = this.canvas.getContext('2d');
+        //this.panzer = {x: 3, y: 4};
+        this.fieldWidth = 40;
+        this.fieldHeight = 40;
+        this.cellSize = 10;
+        this.canvas.width = (this.fieldWidth + 1)*this.cellSize;
+        this.canvas.height = (this.fieldHeight + 1)*this.cellSize;
+        this.drawField()
+        
+    }
+
+    createHtmlElements(){
+        console.log(2);
+        let arrOfHtmlElements = [];
+
+        let div = document.createElement('div');
+        let canv = document.createElement('canvas');
+        canv.id = 'canvas';
+        canv.style = 'display: block;';
+        this.canvas = canv;       
+        
+        
+        arrOfHtmlElements.push(canv);
+
+        for(let n = 0; n < arrOfHtmlElements.length; n++){
+            div.appendChild(arrOfHtmlElements[n])
+        }
+        document.body.appendChild(div);
+    }
+
+    drawField(){
+        const img = new Image();
+        img.src = "cod4.ico";
+        this.ctx.drawImage(img, 5, 5);
+        this.ctx.strokeStyle = 'blue';
+        this.ctx.strokeRect(0, 0, (this.fieldWidth + 1)*this.cellSize, (this.fieldHeight + 1)*this.cellSize);
+        console.log(1);
+
+        this.ctx.fillStyle = 'magenta';
+        this.ctx.fillRect(this.panzer.x*this.cellSize, this.panzer.y*this.cellSize, this.cellSize*2, this.cellSize*2);
+        
+    }
+}
+const draw = new Drawing();
