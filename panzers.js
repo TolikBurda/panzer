e5685ae2f985cda9db2af9e5035205cd1cac8c53
TolikBurda <tolik.burda@gmail.com>
2019-05-11 14:45:06 +0300

    Update panzers.js

--- panzers.js
+++ panzers.js
@@ -1,30 +1,44 @@
 class Panzer {
-    constructor(w, h, inputMap){
+    constructor(w, h, inputMap, g){
 
-        this.position = {x: 1, y: 1};// координаты для спавна в переменную вместо 1,1
+        this.position = {x: 10, y: 1};// координаты для спавна в переменную вместо 1,1
         this.currentDirection = {x: 0, y: 0};
         this.fieldWidth = w;
         this.fieldHeight = h;
         this.superInputMap = inputMap;
-        
+        this.draw = g;
+        this.directionChangeIntevalId = null;
     }
 
 
-    movePanzer(){
-        this.position.x = this.position.x + this.currentDirection.x;
-        this.position.y = this.position.y + this.currentDirection.y;
+    movePanzer(directionChange){
+        this.position.x += directionChange.x ;
+
+        this.position.y += directionChange.y ;
+        console.log(this.position.x, this.position.y);
+        
     }
 
     //если новое направление == старому, то двигаемся, если не равно - поворот
     changeDirectionOfPanzer(keyCode){
-        // var directionMap = {
-        //     up : { x: 0, y: -1},
-        //     down: { x: 0, y: 1},
-        //     right: { x: 1, y: 0},
-        //     left: { x: -1, y: 0}
-        // }   
-        // let directionChange = directionMap[this.superInputMap[keyCode]]
+        var directionMap = {
+            up : { x: 0, y: -1},
+            down: { x: 0, y: 1},
+            right: { x: 1, y: 0},
+            left: { x: -1, y: 0}
+        }   
+        let directionChange = directionMap[this.superInputMap[keyCode]]
+        
+        
+        this.stopChangeDirection()
+        this.directionChangeIntevalId = setInterval(()=>{
+            this.movePanzer(directionChange);   
+            this.draw.drawField(this)
 
+        }, 1000/60);
+    }
+    stopChangeDirection(){
+        clearInterval(this.directionChangeIntevalId);
     }
 
     createAmmo(){
@@ -41,11 +55,13 @@ class Panzer {
 }
 //const panzer = new Panzer()
 
-// class Shell {
-//     constructor(){
-//         this.shellCoords = {x: 0, y: 0};
-//     }
-// }
+class Shell {
+    constructor(){
+        this.shellCoords = {x: 0, y: 0};
+    }
+
+    
+}
 // class Field {
 //     constructor(){
 //         this.arrOfBlocks = [];
@@ -62,10 +78,12 @@ class Panzer {
 class Game {
     constructor(){
         this.draw = new Drawing();
-        
+        this.gameControl();
+        this.intervalId = null;
         this.arrOfPanzers = [];
-        
-
+        this.fieldWidth = 40;
+        this.fieldHeight = 40;
+        this.timer = 200;
         this.superInputMap = [
             {
                 38 : 'up',
@@ -85,7 +103,7 @@ class Game {
     }
 
     createPanzers(){
-        const panz = new Panzer(this.draw.fieldWidth, this.draw.fieldHeight, this.superInputMap[0]);
+        const panz = new Panzer(this.fieldWidth, this.fieldHeight, this.superInputMap[0], this.draw);
         //const panz1 = new Panzer(this.draw.fieldWidth, this.draw.fieldHeight, this.superInputMap[1]);
         this.arrOfPanzers.push(panz);
         //this.arrOfPanzers.push(panz1);
@@ -99,8 +117,31 @@ class Game {
                 //this.arrOfPanzers[i].shooting(e.keyCode)
             }
         })
+        document.addEventListener('keyup', (e)=>{
+            for(let i = 0; i < this.arrOfPanzers.length; i++){
+                this.arrOfPanzers[i].stopChangeDirection()
+            }    
+        })
     }
+    stopGame(){
 
+
+    }
+    startGame(){
+        this.stopGame();
+        this.createPanzers();
+        this.panzerControl();
+        this.draw.drawField(this)
+    }
+    gameControl(){
+        this.draw.startButton.addEventListener('click', ()=>{                        ///()=>
+            this.startGame();  
+        } );
+
+        this.draw.stopButton.addEventListener('click', ()=>{
+            this.stopGame();
+        } );
+    }
     mainLoop(){
 
     }
@@ -110,6 +151,8 @@ class Game {
 class Drawing {
     constructor(){
         this.canvas = null;
+        this.startButton = null;
+        this.stopButton = null;
         this.createHtmlElements();
         this.ctx = this.canvas.getContext('2d');
         this.fieldWidth = 40;
@@ -117,11 +160,9 @@ class Drawing {
         this.cellSize = 10;
         this.scale = this.cellSize * 3;
 
-        //this.panzer = {x: 1, y: 1}
-
         this.canvas.width = (this.fieldWidth + 1)*this.cellSize;
         this.canvas.height = (this.fieldHeight + 1)*this.cellSize;
-        // this.drawField()
+        //this.drawField()
         
     }
 
@@ -130,12 +171,23 @@ class Drawing {
         let arrOfHtmlElements = [];
 
         let div = document.createElement('div');
+        let startGame = document.createElement('input');
+        startGame.type = 'button';
+        startGame.id = 'start';
+        startGame.value = 'start game';
+        this.startButton = startGame;
+        let endGame = document.createElement('input');
+        endGame.type = 'button';
+        endGame.id = 'end';
+        endGame.value = 'end game';
+        this.stopButton = endGame;
         let canv = document.createElement('canvas');
         canv.id = 'canvas';
         canv.style = 'display: block;';
         this.canvas = canv;       
         
-        
+        arrOfHtmlElements.push(startGame);
+        arrOfHtmlElements.push(endGame);
         arrOfHtmlElements.push(canv);
 
         for(let n = 0; n < arrOfHtmlElements.length; n++){
@@ -144,28 +196,31 @@ class Drawing {
         document.body.appendChild(div);
         console.log(2);
     }
-    // spawnPanzer(){ 
-    //     this.panzer.x = Math.round(Math.random() * this.fieldWidth);
-    //     this.panzer.y = Math.round(Math.random() * this.fieldHeight);     
-    // }
-    drawField(game){
-        //this.spawnPanzer()
-        
+  
+    drawField(){
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
         const img = new Image();
         img.src = "hui.png";
         
-        console.log(game.arrOfPanzers);
         
-        // for(let j = 0; j < game.arrOfPanzers.length; j++){
-        //     let panzerPosition = game.arrOfPanzers[j]
-        //     img.addEventListener('load', ()=>{
-        //         this.ctx.drawImage(img, panzerPosition.position.x, panzerPosition.position.y, this.scale, this.scale);// передать сюда координаты 
-        //         console.log('img loaded');
-                
-        //     })
-
-        // }
-
+        for(let j = 0; j < game.arrOfPanzers.length; j++){
+            let panzerPosition = game.arrOfPanzers[j]
+            img.addEventListener('load', ()=>{
+                // let dx = 64;
+                // let dy = 64;
+                // this.ctx.save();
+                // this.ctx.translate(dx, dy);
+                // this.ctx.rotate(30 * (Math.PI/180));
+                // this.ctx.translate(-dx, -dy);
+                // this.ctx.rotate(5 * (Math.PI/180));
+                this.ctx.drawImage(img, panzerPosition.position.x, panzerPosition.position.y, this.scale, this.scale);// передать сюда координаты 
+                console.log('img loaded');
+                this.ctx.restore();
+            })
+        }
+        
 
         // const img = new Image();
         // img.src = "hui.png";
@@ -181,14 +236,6 @@ class Drawing {
         this.ctx.strokeRect(0, 0, (this.fieldWidth + 1)*this.cellSize, (this.fieldHeight + 1)*this.cellSize);
         console.log(1);
 
-
-        // this.ctx.strokeStyle = 'magenta';
-        // for(let j = 0; j < game.arrOfPanzers.length; j++){
-        //     let panz = game.arrOfPanzers[j]
-        //     for(let i = 0; i < panz.segments.length; i++){
-        //         this.ctx.strokeRect(panz.segments[i].x*this.cellSize, panz.segments[i].y*this.cellSize, this.cellSize, this.cellSize);
-        //     }
-        // }
 
 
         // this.ctx.fillStyle = 'magenta';
