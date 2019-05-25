e72f2599e0437da11ed3b48442fcbde022387258
TolikBurda <tolik.burda@gmail.com>
2019-05-25 21:27:57 +0300

    Update panzers.js

--- panzers.js
+++ panzers.js
@@ -5,13 +5,13 @@ class Panzer {
         this.currentDirection = {x: 0, y: 0};
         this.superInputMap = inputMap;
         this.directionChangeIntevalId = null;
-        //this.arrOfShells = [];
     }
 
 
     movePanzer(directionChange){
         this.position.x += directionChange.x ;
         this.position.y += directionChange.y ; 
+        console.log(this.position);
     }
 
     changeDirectionOfPanzer(keyCode){
@@ -21,71 +21,54 @@ class Panzer {
             right: { x: 1, y: 0},
             left: { x: -1, y: 0}
         }   
+        if(this.superInputMap[keyCode] == 'shot'){
+            this.shoting()
+        }
         let directionChange = directionMap[this.superInputMap[keyCode]]
+        //console.log(this.superInputMap[keyCode]);
         
         if(!directionChange){
             return null
         }
         if (directionChange.x == this.currentDirection.x && directionChange.y == this.currentDirection.y){
-            //this.movePanzer()
-            //this.stopChangeDirection()
-            
+            this.movePanzer(directionChange);
 
         }else if(directionChange.x != this.currentDirection.x || directionChange.y != this.currentDirection.y){
-            // this.rotatePanzer(keyCode)
             this.currentDirection = directionChange;   
         }
         
     }
-    // stopChangeDirection(){
-    //     clearInterval(this.directionChangeIntevalId);
-    // } 
-
 
     update(){
         
     }
-    
-    // moveShells(){
-    //     for(let i = 0; i < this.arrOfShells.length; i++){
-    //         let shell = this.arrOfShells[i];
-    //         shell.shellCoords.x += this.currentDirection.x;
-    //         shell.shellCoords.y += this.currentDirection.y;
-    //     }
-    //}
-    // createShells(){
-    //     const shell = new Shell (this.position, this.currentDirection, this.draw)
-    //     this.arrOfShells.push(shell)
-    // }
 
-    shooting(eventKeyCode){
-        if(eventKeyCode == 80){
-            this.pubsub.shootingEvent(eventKeyCode)
-            //this.createShells()80 == p;79 == o
+    shoting(){
+        let offsetPosition = {
+            x : this.position.x + 67*this.currentDirection.x,
+            y : this.position.y + 67*this.currentDirection.y
         }
+        this.pubsub.fireEvent('shot', {position: offsetPosition, direction: this.currentDirection})
+        //this.createShells()80 == p;79 == o
     }
     
 }
 
 class Shell {
-    constructor(p, c, d){
-        this.position = p
-        this.currentDirection = c;
-        this.draw = d;
-        this.shellCoords = {x: 0, y: 0};
-        this.createShell()
+    constructor(coords){
+        this.currentDirection = coords.direction;
+        this.coords = coords.position;
+        this.createShell();
     }
 
     createShell(){
-        // this.shellCoords.x = this.position.x + 67*this.currentDirection.x;
-        // this.shellCoords.y = this.position.y + 67*this.currentDirection.y;
-        // console.log(this.shellCoords);  
-        //this.moveShells()  
+        console.log(this.coords.x, this.coords.y);
+        this.moveShells()  
     }
 
     moveShells(){
-        this.shellCoords.x += this.currentDirection.x;
-        this.shellCoords.y += this.currentDirection.y; 
+        this.coords.x += this.currentDirection.x;
+        this.coords.y += this.currentDirection.y; 
     }
 
 }
@@ -107,7 +90,7 @@ class Shell {
 class Game {
     constructor(pubsub){
         this.pubsub = pubsub; ////Подписываешься на ивент стрельбы в игре/// В танке кидаешь ивент, когда жмёшь кнопку
-        this.pubsub.subscribe(80, this, this.createShells);
+        this.pubsub.subscribe('shot', this, this.createShells);
         this.draw = new Drawing();
         this.gameControl();
         this.intervalId = null;
@@ -119,58 +102,54 @@ class Game {
                 38 : 'up',
                 40 : 'down',
                 39 : 'right',
-                37 : 'left'   //добавить выстрелы в мапу, так как создается по два снаряда
+                37 : 'left',
+                79 : 'shot'
             },
             {
                 87 : 'up',
                 83 : 'down',
                 68 : 'right',
-                65 : 'left'
+                65 : 'left',
+                80 : 'shot'
             }
         ]
     }
 
     createPanzers(){
         const panz = new Panzer(this.superInputMap[0], this.pubsub);
-        //const panz1 = new Panzer(this.superInputMap[1], this.pubsub);
+        const panz1 = new Panzer(this.superInputMap[1], this.pubsub);
         this.arrOfPanzers.push(panz);
-        //this.arrOfPanzers.push(panz1);
-        
+        this.arrOfPanzers.push(panz1);
     }
 
-    createShells(event){
-        if(event){
-            const shell = new Shell();
+    createShells(data){
+        if(data){
+            const shell = new Shell(data);
             this.arrOfShells.push(shell);
-            console.log(this.arrOfShells.length);
-            
+            //console.log(this.arrOfShells.length);
         }
-        
     }
+
     update(){
         for(let i = 0; i < this.arrOfPanzers.length; i++){
             const panz = this.arrOfPanzers[i];
             panz.update();
         }
     }
+
     panzerControl(){
         document.addEventListener('keydown', (e)=> {
             for(let i = 0; i < this.arrOfPanzers.length; i++){
                 this.arrOfPanzers[i].changeDirectionOfPanzer(e.keyCode);
-                this.arrOfPanzers[i].shooting(e.keyCode)
             }
         })
-        // document.addEventListener('keyup', (e)=>{
-        //     for(let i = 0; i < this.arrOfPanzers.length; i++){
-        //         this.arrOfPanzers[i].stopChangeDirection()
-        //     }    
-        // })
     }
+
     stopGame(){
         clearInterval(this.intervalId);
         this.arrOfPanzers = [];
-
     }
+
     startGame(){
         this.stopGame();
         this.createPanzers();
@@ -191,8 +170,8 @@ class Game {
             this.stopGame();
         } );
     }
-    mainLoop(){                                                                     // это не мейн луп а просто движение снарядов 
-        
+    mainLoop(){                   
+
     }
 }
 
@@ -307,19 +286,14 @@ class PubSub {
         var sub = new Subscription(event, obj, method);
         this.subscriptions.push(sub)
     }
-    shootingEvent(eventKeyCode){
+    fireEvent(event, data){
         for (let i = 0; i < this.subscriptions.length; i++) {
             const sub = this.subscriptions[i];
-            if (sub.event == eventKeyCode) {
-                console.log(eventKeyCode);
-                sub.method.call(sub.obj, eventKeyCode)
-                
+            if (sub.event == event) {
+                sub.method.call(sub.obj, data)
             }
         }
     }
 }
 const pubSubInstance = new PubSub();
 const game = new Game(pubSubInstance);
-// const draw = new Drawing(game);
-
-
